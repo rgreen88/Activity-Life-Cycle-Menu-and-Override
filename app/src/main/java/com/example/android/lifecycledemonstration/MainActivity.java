d98c2cfd084f34b6843a4e88532264def485200a
rgreen88 <rynelgreen@gmail.com>
2018-02-22 18:51:28 -0500

    added menu on action bar top-right corner bare bones

--- app/src/main/java/com/example/android/lifecycledemonstration/MainActivity.java
+++ app/src/main/java/com/example/android/lifecycledemonstration/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.lifecycledemonstration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toast.makeText(this, "In onCreate", Toast.LENGTH_SHORT).show();
         Log.i("info", "In onCreate");
 
@@ -26,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         Log.i("info", "onStart: ");
 
         //occasional use instances
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu); //your file name
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
